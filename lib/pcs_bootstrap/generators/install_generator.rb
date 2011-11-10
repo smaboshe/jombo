e9b2cc201f939ec62e73740805d41452e96650b7
Silumesii Maboshe <silumesii@pencilcasestudios.com>
2011-11-10 16:06:48 +0200

    Updating namespace from 'pcs_bootstrap' to 'bootstrap'.

--- lib/pcs_bootstrap/generators/install_generator.rb
+++ lib/pcs_bootstrap/generators/install_generator.rb
@@ -6,8 +6,8 @@ module PcsBootstrap
       source_root File.join(File.dirname(__FILE__), 'templates')
       
       def add_assets
-        insert_into_file "app/assets/javascripts/application.js", "//= require pcs_bootstrap\n", :after => "jquery_ujs\n"
-        insert_into_file "app/assets/stylesheets/application.css", " *= require pcs_bootstrap\n", :after => "require_self\n"
+        insert_into_file "app/assets/javascripts/application.js", "//= require bootstrap\n", :after => "jquery_ujs\n"
+        insert_into_file "app/assets/stylesheets/application.css", " *= require bootstrap\n", :after => "require_self\n"
       end
     end
   end
