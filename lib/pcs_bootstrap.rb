33d134e21980ba4ef1b7e4838793223a6e63fe9d
Silumesii Maboshe <silumesii@pencilcasestudios.com>
2011-11-10 14:56:23 +0200

    Added an install generator for Rails.

--- lib/pcs_bootstrap.rb
+++ lib/pcs_bootstrap.rb
@@ -1,6 +1,8 @@
 require "pcs_bootstrap/version"
+require "pcs_bootstrap/generators/install_generator"
 
 module PcsBootstrap
   class Engine < Rails::Engine
+    # Allows Rails to automatically include assets
   end
 end
